bd9d4e9ee19826b4fa1c2cb2e7353d3c8eded3e1
Napolov Vlad <85844885+Desine@users.noreply.github.com>
2024-11-06 12:08:50 +0300

    added task completing
    i does not work for icon

--- src/components/Todo.jsx
+++ src/components/Todo.jsx
@@ -1,11 +1,28 @@
 import React from "react";
 
 export default function Todo({ todo, setTodos, todos }) {
+  function completeTodo(todo) {
+    let newTodos = todos.map(task =>{
+      if(task.id === todo.id){
+        return {...task, completed: !todo.completed}
+      }
+      return task
+    })
+    setTodos(newTodos)
+  }
+
+
+
   return (
     <div className="todo">
       <div className={todo.completed ? 'completed' : ''}>
-        <input type="checkbox" checked={todo.completed} className="checkbox"/>
+        <input 
+        type="checkbox" 
+        checked={todo.completed}
+         className="checkbox"
+         onChange={()=>completeTodo(todo)}/>
         <label htmlFor={`check-${todo.id}`}>{todo.title}</label>
+        <i className="icon" width='30' height='30' src='https://img.icons8.com/?size=30&id=99933&format=png&color=000000'/>
       </div>
     </div>
   );
