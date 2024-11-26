e18aad00226c39010501dcc1dedbbaefb569a07f
Napolov Vlad <85844885+Desine@users.noreply.github.com>
2024-11-26 11:41:47 +0300

    fetch & axios

--- src/index.js
+++ src/index.js
@@ -1,7 +1,29 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import Main from "./pages/Main";
-import './static/style.css'
+import "./static/style.css";
+import axios from "axios";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
+//root.render(<Main />);
 root.render(<Main />);
+
+
+
+
+
+// let request = fetch("https://jsonplaceholder.typicode.com/todos",{
+// method: 'POST',
+// headers:{
+// 'Content-Type': 'application/json'
+// },
+// body: JSON.stringify({
+// title: "New todo",
+// completed: false,
+// userId: 1
+// })
+// });
+// request
+// .then((response) => response.json())
+// .then((data) => console.log(data)
+// .catch((e) => console.log(e)));
